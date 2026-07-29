4115892d13c266e35e133321598155029d580d47
MChuvilevM <mishanyasha324@gmail.com>
2026-07-29 21:49:58 +0300

    feat(monitoring): update SystemMetricSerializer to accept host name instead of pk

--- monitoring/serializers.py
+++ monitoring/serializers.py
@@ -7,6 +7,11 @@ class HostSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class SystemMetricSerializer(serializers.ModelSerializer):
+    host = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=Host.objects.all()
+    )
+
     class Meta:
         model = SystemMetric
         fields = '__all__'
