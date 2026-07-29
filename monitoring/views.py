04fbc2cd0df72e908eed0da22b4b276d714cf0ad
MChuvilevM <mishanyasha324@gmail.com>
2026-07-29 21:37:09 +0300

    feat(monitoring): add api_metrics view function

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Host, SystemMetric
 from .serializers import HostSerializer, SystemMetricSerializer
 
@@ -9,3 +11,9 @@ class HostViewSet(viewsets.ModelViewSet):
 class SystemMetricViewSet(viewsets.ModelViewSet):
     queryset = SystemMetric.objects.all()
     serializer_class = SystemMetricSerializer
+
+@api_view(['GET'])
+def api_metrics(request):
+    metrics = SystemMetric.objects.all()
+    serializer = SystemMetricSerializer(metrics, many=True)
+    return Response(serializer.data)
